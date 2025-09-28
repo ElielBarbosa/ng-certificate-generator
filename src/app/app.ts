3b203a6337172f714d745e97cdff966c8eedf929
Eliel Barbosa <elielbarbosads@gmail.com>
2025-09-27 22:02:39 -0300

    feat(ui): Criar componentes reutilizáveis

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificate } from './_components/item-certificate/item-certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificate],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
